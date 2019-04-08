0cabc73b95fae17be6e468483d3a39fefba18da8
Ever Daniel Barreto <everdaniel@gmail.com>
2019-04-08 16:56:38 -0400

    Add main game control

---
+++ game.php
@@ -0,0 +1,8 @@
+<?php
+
+// Main Game Control
+while (($command = readline("Enter your option or enter 'q' to quit: ")) !== 'q') {
+    echo "Command is {$command}\n";
+}
+
+echo "Thanks for playing, good bye!\n";
